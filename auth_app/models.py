da07dcd613bebf734afb9a8b8b1f589427d3a4e4
ramin rahmani <ramin.rahmani971@gmail.com>
2025-04-30 18:48:48 +0330

    fix the bug when createsuperuser by customusermanager

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,10 +1,36 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.password_validation import validate_password as validate_pass 
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
 from django.core.validators import RegexValidator,MinLengthValidator
 from django.conf import settings
 
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('The Phone field must be set')
+        # validate_pass(password)
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('SuperUser must be set is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('SuperUser must be set is_superuser=True')
+        # validate_pass(password)
+
+        return self.create_user(phone, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -16,6 +42,7 @@ class User(AbstractUser):
     username = None
     USERNAME_FIELD = 'phone'
     # REQUIRED_FIELDS = ['email']
+    objects = CustomUserManager()
     
     def __str__(self):
         return self.phone
